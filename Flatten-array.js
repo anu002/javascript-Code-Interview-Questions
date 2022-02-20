cd16ba56d27277b9f32caf18d79c72ace718ab7d
anu002 <anu.bbdnitm@gmail.com>
2022-02-21 00:18:52 +0530

    Create Flatten-array.js

---
+++ Flatten-array.js
@@ -0,0 +1,26 @@
+ const array = [1,[3,7,[8,9]],[7]];
+
+/*Method 1*/
+array.flat(4);
+ // Method 2
+ console.log(flattenArray(array));
+ function flattenArray(array)
+ {
+    let Flat =[];
+    for(let i=0;i<array.length;i++)
+    {
+      if(Array.isArray(array[i]))
+      {
+        //const flatData=;
+       Flat = Flat.concat( flattenArray(array[i]));
+        
+      }
+      else
+      {
+       Flat.push(array[i]);
+      
+      }
+    }
+    return Flat;
+     
+ }
